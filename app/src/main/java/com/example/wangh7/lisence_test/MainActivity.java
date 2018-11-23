2bee541f5fd3d67ae729f356a6a3ebb4f0d82f03
wangh7 <978102365@qq.com>
2018-11-23 13:16:08 +0800

    Test

---
+++ app/src/main/java/com/example/wangh7/lisence_test/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.wangh7.lisence_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void test(){}
+}
